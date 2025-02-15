0fc5e59b20826d92d3017f8b4ea4c3f632c15c4f
Eitan Rashkovan <eitan.rashkovan@gmail.com>
2025-02-14 17:19:19 -0800

    adding a more info tab to expand on places

--- Overlap/app/_layout.tsx
+++ Overlap/app/_layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout() {
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="morefilters" options={{ title: 'More Filters', headerShown: false }} />
+        <Stack.Screen name="moreInfo" options={{ title: 'More Info', headerShown: false }} />
       </Stack>
     </FiltersProvider>
   );
